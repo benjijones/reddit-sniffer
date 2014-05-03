179b7d1b663d3b70ca9622d628082a4b00450a09
Benji Jones <benji.s.jones@gmail.com>
2014-05-02 23:52:00 -0400

    add sbt build definitions

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "reddit-sniffer"
+
+version := "1.0"
+
+scalaVersion := "2.10.3"
---
+++ project/Build.scala
@@ -0,0 +1,13 @@
+import sbt._
+
+
+object MyBuild extends Build {
+
+  lazy val root = Project("reddit-sniffer", file("."))
+//                    .dependsOn(redditProject)
+		    .dependsOn(reddheadProject)
+
+  //lazy val redditProject = RootProject(uri("git://github.com/benjijones/reddit-sniffer.git"))
+  lazy val reddheadProject = RootProject(file("/workspace/reddhead"))
+  
+}
